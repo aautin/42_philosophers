5e2e0931dc05117210ff1c7a86931b912a8860f4
aautin <ezagof@gmail.com>
2024-02-03 00:23:46 +0100

    Continue to test the subject's new functions
    * mutex : typedef, ..mutex_init, ..mutex_destroy, ..lock, ..unlock
    	- tryied to thread without and with mutex
    	  to detect data-races
    * create threads in loops : how to wait (pthread_join) them ?
    	- watch the error to create and wait in single same loop
    	- create to successive loops (first create, seconde join)
    * init the gitignore and 'git rm -r --cached .vscode/'

--- main.c
+++ main.c
@@ -14,13 +14,15 @@
 // pthread_create, pthread_detach, pthread_join, pthread_mutex_init,
 // pthread_mutex_destroy, pthread_mutex_lock, pthread_mutex_unlock
 
+int global_variable;
+
 void	*thread_routine(void *adress)
 {
 	struct timeval	time_save;
 
 	usleep(200);
 	gettimeofday(&time_save, NULL);
-	printf("thread_routine:\t\t%ld\n", time_save.tv_usec - ((struct timeval *)adress)->tv_usec);
+	printf("thread_routine:\t\t\t\t%ld\n", time_save.tv_usec - ((struct timeval *)adress)->tv_usec);
 	pthread_exit("fin du thread");
 }
 
@@ -37,6 +39,41 @@ void	*func2(void *i)
 	pthread_exit("fin du thread");
 }
 
+void	*incrementation(void *i)
+{
+	for (int j = 0; j < 100000; j++)
+		*((int *) i) = *((int *) i) + 1;
+	pthread_exit(NULL);
+}
+
+void	*decrementation(void *i)
+{
+	for (int j = 0; j < 100000; j++)
+		*((int *) i) = *((int *) i) - 1;
+	pthread_exit(NULL);
+}
+
+void	*incrementation_mutex(void *i)
+{
+	for (int j = 0; j < 100000; j++)
+	{
+		pthread_mutex_lock((pthread_mutex_t *) i);
+		global_variable++;
+		pthread_mutex_unlock((pthread_mutex_t *) i);
+	}
+	pthread_exit(NULL);
+}
+
+void	*decrementation_mutex(void *i)
+{
+	for (int j = 0; j < 100000; j++)
+	{
+		pthread_mutex_lock((pthread_mutex_t *) i);
+		global_variable--;
+		pthread_mutex_unlock((pthread_mutex_t *) i);
+	}
+	pthread_exit(NULL);
+}
 
 int	main(int argc, char *argv[])
 {
@@ -48,8 +85,8 @@ int	main(int argc, char *argv[])
 	int	time_save = time.tv_usec;
 	if (time.tv_usec != 0)
 		gettimeofday(&time, NULL);
-	printf("day microtime\t\t%ld\n", time.tv_usec);
-	printf("exec microtime\t\t%ld\n", time.tv_usec - time_save);
+	printf("day microtime\t\t\t\t%ld\n", time.tv_usec);
+	printf("exec microtime\t\t\t\t%ld\n\n", time.tv_usec - time_save);
 
 
 
@@ -57,13 +94,13 @@ int	main(int argc, char *argv[])
 	int nb = 0;
 
 	memset(&nb, 255, 1);
-	printf("nb\t\t\t%d\n", nb);
+	printf("nb\t\t\t\t\t%d\n", nb);
 	memset(&nb, 255, 2);
-	printf("nb\t\t\t%d\n", nb);
+	printf("nb\t\t\t\t\t%d\n", nb);
 	memset(&nb, 255, 3);
-	printf("nb\t\t\t%d\n", nb);
+	printf("nb\t\t\t\t\t%d\n", nb);
 	memset(&nb, 255, 4);
-	printf("nb\t\t\t%d\n", nb);
+	printf("nb\t\t\t\t\t%d\n\n", nb);
 
 
 
@@ -73,12 +110,12 @@ int	main(int argc, char *argv[])
 
 	usleep(1000);
 	gettimeofday(&time, NULL);
-	printf("time spent\t\t%ld\n", time.tv_usec - time_save);
+	printf("time spent\t\t\t\t%ld\n", time.tv_usec - time_save);
 	time_save = time.tv_usec;
 
 	usleep(2000);
 	gettimeofday(&time, NULL);
-	printf("time spent\t\t%ld\n", time.tv_usec - time_save);
+	printf("time spent\t\t\t\t%ld\n\n", time.tv_usec - time_save);
 
 
 
@@ -87,7 +124,7 @@ int	main(int argc, char *argv[])
 	char		*return_val;
 	pthread_create(&thread, NULL, &thread_routine, &time);
 	pthread_join(thread, (void **) &return_val);
-	printf("return_value\t\t%s\n", return_val);
+	printf("return_value\t\t\t\t%s\n\n", return_val);
 
 
 
@@ -99,14 +136,71 @@ int	main(int argc, char *argv[])
 	pthread_create(&thread2, NULL, &func2, &i);
 
 	pthread_join(thread1, NULL);
-	printf("i after thread1 ended\t%d\n", i);
+	printf("i after thread1 ended\t\t\t%d\n", i);
 	pthread_join(thread2, NULL);
-	printf("i after thread2 ended\t%d\n", i);
+	printf("i after thread2 ended\t\t\t%d\n\n", i);
 
 
 
-	// the threads trying to access to the same variable at the same time
-	
+	// threads accessing to the same variable at the same time (without-mutex)
+	pthread_t	thread3;
+	pthread_t	thread4;
+	int number = 0;
+	pthread_create(&thread3, NULL, &incrementation, &number);
+	pthread_create(&thread4, NULL, &decrementation, &number);
+
+	pthread_join(thread3, NULL);
+	pthread_join(thread4, NULL);
+	printf("(without-mutex) data races's number\t%d\n", number);
+	// threads accessing to the same variable at the same time (with-mutex)
+	pthread_t	thread5;
+	pthread_t	thread6;
+	pthread_mutex_t	mutex;
+	pthread_mutex_init(&mutex, NULL);
+	global_variable = 0;
+	pthread_create(&thread5, NULL, &incrementation_mutex, &mutex);
+	pthread_create(&thread6, NULL, &decrementation_mutex, &mutex);
+
+	pthread_join(thread5, NULL);
+	pthread_join(thread6, NULL);
+	printf("(with-mutex) data races's number\t%d\n\n", global_variable);
+	pthread_mutex_destroy(&mutex);
+
+
+
+	// threads created in a loop (wrong version)
+	pthread_t	th[2];
+	pthread_mutex_t	mutex_2;
+	pthread_mutex_init(&mutex_2, NULL);
+	global_variable = 0;
+	int index;
+	for (index = 0; index < 2; index++)
+	{
+		pthread_create(&th[index], NULL, &incrementation_mutex, &mutex_2);
+		printf("thread[%d] has been created\n", index);
+		pthread_join(th[index], NULL);
+		printf("finished to wait thread[%d]\n", index);
+	}
+	printf("(wrong loop) data races's number\t%d\n\n", global_variable - 200000);
+	pthread_mutex_destroy(&mutex_2);
+	// threads created in a loop (right version)
+	pthread_t	th2[2];
+	pthread_mutex_t	mutex_3;
+	pthread_mutex_init(&mutex_3, NULL);
+	global_variable = 0;
+	for (index = 0; index < 2; index++)
+	{
+		pthread_create(&th2[index], NULL, &incrementation_mutex, &mutex_3);
+		printf("thread[%d] has been created\n", index);
+	}
+	for (index = 0; index < 2; index++)
+	{
+		pthread_join(th2[index], NULL);
+		printf("finished to wait thread[%d]\n", index);
+	}
+	printf("(right loops) data races's number\t%d\n\n", global_variable - 200000);
+	pthread_mutex_destroy(&mutex_3);
+
 	return (0);
 }
  
